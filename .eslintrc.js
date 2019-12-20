bc8e5ead55c7ee21c54c551410e31834675a9106
guilhermekonell <gui.ri.konell@hotmail.com>
2019-12-19 21:49:50 -0300

    Adicionando itens duplicados ao carrinho

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,6 +36,7 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'jsx-a11y/control-has-associated-label': 'off',
     'react/state-in-constructor': 'off',
-    'no-console': ["error", { allow: ["tron"] }]
+    'no-console': ["error", { allow: ["tron"] }],
+    'no-param-reassign': 'off',
   },
 };

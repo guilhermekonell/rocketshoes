b9d72dbba2bacab892b8062ae98bb6c553e8d069
guilhermekonell <gui.ri.konell@hotmail.com>
2020-02-14 00:18:18 -0300

    Convertendo classes para function - React Hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,8 @@ module.exports = {
   },
   plugins: [
     'react',
-    'prettier'
+    'prettier',
+    'react-hooks'
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -38,5 +39,7 @@ module.exports = {
     'react/state-in-constructor': 'off',
     'no-console': ["error", { allow: ["tron"] }],
     'no-param-reassign': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   },
 };
